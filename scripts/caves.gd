e80d1fa060bc383c683d62627e7bd5f142c1cb2a
Lucas Daniel <lucasdmaciel12@gmail.com>
2025-11-13 15:42:44 -0300

    Implementei alguns fade_transitions entre as fases
    Fade_transitions implementadas

--- scripts/caves.gd
+++ scripts/caves.gd
@@ -6,3 +6,5 @@ extends Node2D
 func _ready() -> void:
 	if !BackgroundMusic.playing:
 		BackgroundMusic.play()
+	$Fade_transition.show()
+	$Fade_transition/AnimationPlayer.play("fade_out")
--- scripts/levelend.gd
+++ scripts/levelend.gd
@@ -6,4 +6,8 @@ func _on_body_entered(_body: Node2D) -> void:
 	call_deferred("load_next_scene")
 
 func load_next_scene():
+	var trans = get_tree().get_nodes_in_group("transição")[0]
+	trans.show()
+	trans.get_child(0).play("fade_in")
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://scene/" + next_level + ".tscn")
